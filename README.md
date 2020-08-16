8d48e4bd3250119956a82cf7600670c794300323
saurabh <50881320+T3CHMYTH@users.noreply.github.com>
2020-08-16 16:51:30 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+## What it is?
+This is a matrix program written for windows command prompt
+
+## How to install?
+Just Download the zip file from the github and then extract the zip.
+
+Then just execute the file extracted (by double-click from the mouse)
+
+#written by T3CHMYTH (saurabh)
---
+++ win_matrix.bat
@@ -0,0 +1,17 @@
+echo off
+title Matrix_From_T3CHMYTH
+color 0a
+mode 1000
+goto welcome
+
+welcome:
+cls
+echo WELCOME TO MATRIX WORLD.............
+pause
+cls
+goto matrix
+
+matrix:
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+PING 1.1.1.1 -n 1 -w 0.3 > NUL
+goto matrix
